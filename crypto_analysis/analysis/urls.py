f8cc1a6e173cd6d4d6cb3492d06adaba2e771433
Vishwa_Priyan <vishwapriyan21@gmail.com>
2025-02-11 12:41:25 +0530

    latest update

--- crypto_analysis/analysis/urls.py
+++ crypto_analysis/analysis/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import home
+from . import views
 
 urlpatterns = [
-    path('', home, name='home'),  # Homepage route
+    path('', views.home, name='home'),
+    path('analyze_transactions/', views.analyze_transactions_view, name='analyze_transactions'),
+    path('about/', views.about, name='about'),
+    path('generate-pdf/<str:wallet_address>/', views.generate_pdf_report, name='generate_pdf'),
 ]
